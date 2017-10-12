b384bfd6c76e917dd64c300953c345cc79143483
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-11 23:21:41 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace arrays_2d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] text =
+            {
+                {"one", "two", "three"},
+                {"fish", "cats", "dogs"}
+            };
+            
+            Console.WriteLine(text[1,2]);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# 2D Arrays
+
+Code Kata.
+
+Its in the console. Just some more reps. 
